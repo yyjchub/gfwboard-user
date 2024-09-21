01e4d249df04c8a4819d04793a8e159d27baa927
yyjchub <advertservices@protonmail.com>
2024-09-21 08:23:21 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ const config: DefaultConfigProps = {
   background_url: "https://unsplash.com/random",
   description: "天下武功 唯快不破",
   logo: "",
-  api: "https://sub.crosswalltech.com",
+  api: "https://houduan.fastss.de",
   languages: ["zh-CN", "en-US"],
   googleAnalytics: {
     measurementId: getGA()
