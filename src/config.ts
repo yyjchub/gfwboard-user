dfee502a523f8eba50f7144a30c45b39177d9a3d
yyjchub <advertservices@protonmail.com>
2024-09-23 12:22:38 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ const config: DefaultConfigProps = {
   themeDirection: "ltr",
   title: "跨越长城",
   title_split: " - ",
-  background_url: "https://unsplash.com/random",
+  background_url: "https://houduan.fastss.de",
   description: "天下武功 唯快不破",
   logo: "",
   api: "https://houduan.fastss.de",
